eadee9ddfadac7528d39dc5bd6f0de4ca80ff7bc
Santhan07 <108226308+Santhan07@users.noreply.github.com>
2022-07-26 08:36:41 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(
+  <>
+    <App />
+  </>,
+  document.getElementById("root")
+);
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
